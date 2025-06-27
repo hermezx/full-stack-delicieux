f0748fdfeeb99805493130bf7132cc3a9e3c3c54
hermezx <159937786+hermezx@users.noreply.github.com>
2025-06-27 22:56:47 +0800

    Configure Railway DATABASE_URL and add dj-database-url

--- back-end/BackEndProject/settings.py
+++ back-end/BackEndProject/settings.py
@@ -78,18 +78,10 @@ WSGI_APPLICATION = 'BackEndProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'backendproject',
-        'USER': 'root',
-        'PASSWORD': 'root@123!',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-            }
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
