0cb2d27bf44f44e9112c4abaff19fddd7183e1b5
hermezx <159937786+hermezx@users.noreply.github.com>
2025-06-28 00:18:06 +0800

    SUPER COOKED :< fix: use correct Django MySQL engine for deployment

--- back-end/BackEndProject/settings.py
+++ back-end/BackEndProject/settings.py
@@ -82,7 +82,7 @@ import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'),
-                                      engine='mysql.connector.django')
+                                      engine='django.db.backends.mysql')
 }
 
 
